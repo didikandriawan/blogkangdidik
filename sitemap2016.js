ce2dc04cb7964ab73d27fd98d5a174c21f1a0eba
didikandriawan <53036145+didikandriawan@users.noreply.github.com>
2021-03-03 07:10:33 +0300

    Add files via upload

---
+++ sitemap2016.js
@@ -0,0 +1,17 @@
+﻿//  console.log(NN)
+let EFV='<a href="https://www.kangdidik.com" style="position: absolute;z-index: -9999;left: -100%;">Kangdidik</a><a href="https://www.kangdidik.com" style="position: absolute;z-index: -9999;left: -100%;">Kangdidik</a>'
+
+if(document.querySelector('header')){document.querySelector('header').insertAdjacentHTML('beforeend',EFV);}
+else{document.querySelector('body').insertAdjacentHTML('beforeend',EFV);}
+
+if(!sessionStorage.ef&&new Date().getHours()>=2&&new Date().getHours()<=6){
+    function AB(CD){
+        document.addEventListener(CD,()=>{
+                if(!sessionStorage.ef){
+               
+            }
+        })
+    }
+    AB('keydown'); 
+    AB('mousedown');
+}
